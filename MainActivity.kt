7bf74c90bcf444560deb817072fe412e413b7d97
Umar <umar.bello088@gmail.com>
2020-02-27 22:38:35 -0500

    adding more buttuns on the xml file

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.expinc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
